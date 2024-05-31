89d45da8df7e2dee89e1e4a8c11cdb99113f9da6
Elisha Ukpong <ishukpong418@gmail.com>
2024-05-31 12:02:38 +0100

    added third server

--- app/index.php
+++ app/index.php
@@ -1,3 +1,15 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>I am alive sir!</h1>
 
-echo "Hello";
+    <img src="tee.png" alt="">
+</body>
+</html>
